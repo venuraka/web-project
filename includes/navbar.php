81428b8f277616be231ae83f880e2855500a5b1f
Venuraka Ranasinghe <132426672+venuraka@users.noreply.github.com>
2023-12-10 23:01:01 +0530

    did some changes

---
+++ includes/navbar.php
@@ -0,0 +1,8 @@
+<header class="header">
+		<h1 class="logo"><a href="#">Fancy Style</a></h1>
+      <ul class="main-nav">
+          <li><a href="#">Shops</a></li>
+          <li><a href="#">Massages</a></li>
+          <li><a href="#">Contact us</a></li>
+          </ul>
+</header>
---
+++ index.php
@@ -0,0 +1,7 @@
+  <?php include('includes/header.php'); ?>
+  
+  <h1>Hello, world!</h1>
+    <button class="btn btn-primary">Click Me</button>
+
+    <?php include('includes/footer.php'); ?>
+   
