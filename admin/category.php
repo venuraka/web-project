feed35b85e7fe7e17a0ce33a39de1311d85a0565
Venuraka Ranasinghe <132426672+venuraka@users.noreply.github.com>
2023-12-12 10:48:27 +0530

    new changes

--- admin/category.php
+++ admin/category.php
@@ -4,7 +4,7 @@ include('../middleware/adminmiddleware.php');
  ?>
 <div class="container">
     <div class="row">
-        <div class="col-md-13">
+        <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
                         <h4>Categories</h4>
@@ -18,16 +18,37 @@ include('../middleware/adminmiddleware.php');
                                     <th>Image</th>
                                     <th>Status</th>
                                     <th>Edit</th>
-                                    
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td>shoes</td>
-                                    <td>Visible</td>
-                                    <td>Edit</td>
-                                </tr>
+                                <?php
+                                            $category=getAll("Categories");
+                                            if (mysqli_num_rows($category) > 0) 
+                                            {
+                                                foreach($category as $item)
+                                                {
+                                                    ?>
+                                                    <tr>
+                                                        <td><?= $item['id']; ?></td>
+                                                        <td><?= $item['name']; ?></td>
+                                                        <td>
+                                                        <img src="../uploads/<?= $item['image'];  ?>" width="50px" height="50px" alt="<?= $item['name'];  ?>">
+                                                        </td>
+                                                        <td>
+                                                            <?= $item['status'] == '0' ? "visible" : "hidden"?>
+                                                        </td>
+                                                        <td>
+                                                            <a href="#" class="btn btn-primary">Edit</a>
+                                                        </td>
+                                                        </tr>
+                                                    <?php
+                                                }
+                                            }
+                                            else
+                                            {
+                                                echo "No record found";
+                                            }
+                                ?>
                             </tbody>
                         </table>
                     </div>
@@ -35,5 +56,4 @@ include('../middleware/adminmiddleware.php');
         </div>
     </div>
 </div>
-
 <?php include('adminpannel/footer.php'); ?>
--- functions/myfunctions.php
+++ functions/myfunctions.php
@@ -1,4 +1,11 @@
 <?php
+include('../config/dbcong.php');
+function getAll($table)
+{
+     global $conn;
+    $query = "SELECT * FROM $table";
+    return $query_run = mysqli_query($conn ,$query);
+}
 function redirect($url, $message) 
 {   
         $_SESSION['message'] = $message;
